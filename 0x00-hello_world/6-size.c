51992abd29b489ecaf501d78a997a1af01e68d6f
Lidya68 <lidyawonde68@gmail.com>
2022-12-08 08:26:34 -0800

    not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,4 +17,5 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(l));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(ll));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-	return (0);}	
+	return (0);
+}
