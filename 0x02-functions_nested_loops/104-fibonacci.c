7e456a653209ac1992a2975eecc8c3277b8b2b0b
Lidya68 <lidyawonde68@gmail.com>
2022-12-13 05:48:26 -0800

    Fibonacci numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+  * main - print the first 98 fibonacci numbers.
+  * Return: Nothing.
+  */
+int main(void)
+{
+	int count;
+	unsigned long i, j, k;
+	unsigned long m, n, p, carry;
+
+	count = 0;
+	i = 0;
+	j = 1;
+	for (count = 1; count <= 91; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu, ", k);
+	}
+	m = i % 1000;
+	i = i / 1000;
+	n = j % 1000;
+	j = j / 1000;
+	while (count <= 98)
+	{
+		carry = (m + n) / 1000;
