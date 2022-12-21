4d29dc0cbdeea2857591cfad6e7527adb3967f51
Lidya68 <lidyawonde68@gmail.com>
2022-12-21 11:55:53 -0800

    takes sweat

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,51 +1,23 @@
-#include "main.h"
 #include <stdio.h>
-/**
- * infinite_add - Adds two numbers
- * @n1: first input string
- * @n2: second input string
- * @r: pointer to buffer where result is stored
- * @size_r: requested size for the buffer
- * Return: pointer to buffer where result is stored
- */
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int main(void)
 {
-	int i, j, n, len1, len2; /* i = iterator, n = carry over number */
+int n;
+int a[5];
+int *p;
 
-	for (len1 = 0; n1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; n2[len2] != '\0'; len2++)
-		;
-	if (size_r >= len1 || size_r >= len2)
-		if (len1 >= len2)
-			i = j = len2 - 1;
-		else
-			i = j = len1 - 1;
-	else
-		i = j = size_r;
-	n = 0;
-	/* if first number >= 10, set the value to 1 and increase the buffer by 1.*/
-	if ((n1[0] - '0') + (n2[0] - '0') >= 10)
-	{
-		r[0] = 1 + '0';
-		j = 1;
-	}
-
-	while (i >= 0)
-	{
-		r[j] = (n1[i] - '0') + (n2[i] - '0') + n;
-		if (r[j] >= 10)
-		{
-			r[j] %= 10;
-			n = 1;
-		}
-		else
-			n = 0;
-		r[j] += '0';
-		i--;
-		j--;
-	}
-	r[size_r] = '\0';
-	return (r);
+a[2] = 1024;
+p = &n;
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
+*(p + 5) = 98;
+/* ...so that this prints 98\n */
+printf("a[2] = %d\n", a[2]);
+return (0);
 }
