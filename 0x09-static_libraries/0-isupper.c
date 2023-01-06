b2a3eeebd62e18787d17b6fb759c92175a013407
Lidya68 <lidyawonde68@gmail.com>
2023-01-06 12:10:58 -0800

    A library is not a luxury

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,14 +1,15 @@
 #include "main.h"
+
 /**
- * _isupper - uppercase letters
- * @c: char to check
- *
- * Return: 0 or 1
+ * _isupper - checks for uppercase.
+ * @c: character to check.
+ * Return: 1 if c is uppercase, otherwise return 0.
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
-	return (0);
+		return (0);
 }
--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,11 +1,14 @@
 #include "main.h"
-		
+
 /**
- * _isdigit - checks for a digit (0 through 9)
- * @c: int to be checked
- * Return: 1 if c is a digit, 0 otherwise
+ * _isdigit - checks for digits 1-9
+ * @c: character to check.
+ * Return: 1 if c is a digit, otherwise return 0.
  */
 int _isdigit(int c)
 {
-	return (c >= '0' && c <= '9');
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
 }
