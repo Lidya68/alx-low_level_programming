71dabb59c982fa23a46c387b953e0b35f2dc6297
Lidya68 <lidyawonde68@gmail.com>
2023-01-16 11:29:24 -0800

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef STRTYP
+#define STRTYP
+/**
+ * struct dog - structure
+ * @name: string
+ * @age: float
+ * @owner: characters
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
