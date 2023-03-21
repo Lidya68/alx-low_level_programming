bda00ad1d1b8b7913e6499be8b1e0e2e19fc13b5
Lidya68 <lidyawonde68@gmail.com>
2023-03-21 09:29:27 -0700

    low project

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: char input
+ * Return: length of the input string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i += 1;
+	}
+	return (i);
+}
