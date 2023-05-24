8d1ce84cb9aa4cd2d276f07c89988a74bbaa84f5
Lidya68 <lidyawonde68@gmail.com>
2023-05-24 11:21:18 -0700

    make f file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -22,10 +22,10 @@ def num_water_neighbors(grid, i, j):
 def island_perimeter(grid):
     """Returns the perimeter of the island in grid."""
 
-    perim = 0
+    perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j]:
-                perim += num_water_neighbors(grid, i, j)
+                perimeter += num_water_neighbors(grid, i, j)
 
-    return perim
+    return perimeter
